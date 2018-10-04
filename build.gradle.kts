1fca12cf243478633e1596648584ca3e630a9be7
Michael Stepanov <stepanovmixail@gmail.com>
2018-10-04 23:23:58 +0300

    Add scripts for publishing plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,7 @@ buildscript {
 plugins {
     id("java-gradle-plugin")
     id("org.jetbrains.kotlin.jvm").version("1.2.71")
+    id("com.gradle.plugin-publish").version("0.10.0")
 }
 
 repositories {
@@ -20,4 +21,22 @@ dependencies {
     compile(group = "org.jetbrains.kotlin", name = "kotlin-stdlib-jdk8")
     compile(group = "org.jetbrains.kotlin", name = "kotlin-gradle-plugin", version = "1.2.71")
     compile(group = "com.google.protobuf", name = "protobuf-gradle-plugin", version = "0.8.6")
+}
+
+pluginBundle {
+    tags = listOf("kotlin", "grpc")
+    vcsUrl = "https://github.com/mikhailstepanov88/grpc_kotlin"
+    website = "https://github.com/mikhailstepanov88/grpc_kotlin"
+    description = "Plugin for simplify working with GRPC and kotlin"
+}
+
+gradlePlugin {
+    plugins {
+        create("grpcKotlinPlugin") {
+            id = "com.github.mikhailstepanov88.grpc-kotlin"
+            displayName = "GRPC Kotlin Plugin"
+            description = "Plugin for simplify working with GRPC and kotlin"
+            implementationClass = "com.github.mikhailstepanov88.grpc_kotlin.GrpcKotlinPlugin"
+        }
+    }
 }
